ab318de5d83e1d2220768938cad9c4bc266c8246
Bence-dp <bencediplacido16@gmail.com>
2025-04-11 17:53:51 +0200

    correction commentaire

--- utils/tables_symboles.c
+++ utils/tables_symboles.c
@@ -3,17 +3,17 @@
 #include <string.h>
 #include "tables_symboles.h"
 
-static table_t *pile = NULL; // pile 
+static table_t *pile = NULL; /*  pile  */
 static int pos = 0;
 
-void push_table() { //ajout d'une table de symboles
+void push_table() { /* ajout d'une table de symboles */
     table_t *t = (table_t *)malloc(sizeof(table_t));
     t->symboles = NULL;
     t->suivant = pile;
     pile = t;
 }
 
-void pop_table() { // supprimer la derniere table de symboles de la pile et vider la mem
+void pop_table() { /*  supprimer la derniere table de symboles de la pile et vider la mem */
     if (pile) {
         table_t *tmp = pile;
         pile = pile->suivant;
@@ -21,11 +21,11 @@ void pop_table() { // supprimer la derniere table de symboles de la pile et vide
     }
 }
 
-table_t* top_table() { //sommet de la pile / derniere tables de symboles
+table_t* top_table() { /* sommet de la pile / derniere tables de symboles */
     return pile;
 }
 
-symbole_t* ajouter(table_t *table, char *nom) { //ajouter un symbole a la table
+symbole_t* ajouter(table_t *table, char *nom) { /* ajouter un symbole a la table */
     symbole_t *s = (symbole_t *)malloc(sizeof(symbole_t));
     s->nom = strdup(nom);
     s->suivant = table->symboles;
@@ -33,7 +33,7 @@ symbole_t* ajouter(table_t *table, char *nom) { //ajouter un symbole a la table
     return s;
 }
 
-symbole_t* rechercher(table_t *table, char *nom) {  // rechercher un symbole dans la table
+symbole_t* rechercher(table_t *table, char *nom) {  /*  rechercher un symbole dans la table */
     for (symbole_t *s = table->symboles; s != NULL; s = s->suivant) {
         if (strcmp(s->nom, nom) == 0)
             return s;
@@ -41,7 +41,7 @@ symbole_t* rechercher(table_t *table, char *nom) {  // rechercher un symbole dan
     return NULL;
 }
 
-void supprimer(table_t *table, char *nom) { // supprimer un symbole dans la table et vide la mem alloué
+void supprimer(table_t *table, char *nom) { /*  supprimer un symbole dans la table et vide la mem alloué */
     symbole_t **pp = &table->symboles;
     while (*pp) {
         if (strcmp((*pp)->nom, nom) == 0) {
@@ -55,7 +55,7 @@ void supprimer(table_t *table, char *nom) { // supprimer un symbole dans la tabl
     }
 }
 
-void detruire_table(table_t *table) {  //supprimer une table de symbole et vide la mem alloué
+void detruire_table(table_t *table) {  /* supprimer une table de symbole et vide la mem alloué */
     symbole_t *s = table->symboles;
     while (s) {
         symbole_t *tmp = s;
@@ -66,7 +66,7 @@ void detruire_table(table_t *table) {  //supprimer une table de symbole et vide 
     free(table);
 }
 
-int taille_type(type_t type) {  //taille des differents types
+int taille_type(type_t type) {  /* taille des differents types */
     switch (type) {
         case INT: return 4;
         case STRING: return 8;
--- utils/tables_symboles.h
+++ utils/tables_symboles.h
@@ -3,7 +3,7 @@
 
 typedef enum { INT, STRING } type_t;
 
-typedef struct symbole { //symbole
+typedef struct symbole { /* symbole */
     char *nom;
     type_t type;
     int taille;
@@ -16,13 +16,13 @@ typedef struct table {
     struct table *suivant;
 } table_t;
 
-// Fonctions pour la gestion des tables / symboles
+/* Fonctions pour la gestion des tables / symboles */
 symbole_t* ajouter(table_t *table, char *nom);
 symbole_t* rechercher(table_t *table, char *nom);
 void supprimer(table_t *table, char *nom);
 int taille_type(type_t type);
 
-// Fonctions pour gérer la pile des tables de symboles
+/* Fonctions pour gérer la pile des tables de symboles */
 void push_table();
 void pop_table();
 table_t* top_table();
