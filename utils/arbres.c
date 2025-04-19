f879430bd9a702d0739d114a942dc266410b489e
Bence-dp <bencediplacido16@gmail.com>
2025-04-19 18:48:25 +0200

    arbres concatener des chaines de char et appliquer un style de noeud nécessaire

---
+++ utils/arbres.c
@@ -0,0 +1,66 @@
+#include "arbres.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int numNode = 1;
+
+char *concat(int nbArgs, ...) { /* concatene des chaines de characteres*/
+    va_list args;
+    va_start(args, nbArgs);
+
+    size_t total_length = 0;
+    for (int i = 0; i < nbArgs; i++) {
+        const char *s = va_arg(args, const char *);
+        if (s != NULL) total_length += strlen(s);
+    }
+    va_end(args);
+
+    char *result = (char *)malloc(total_length + 1);
+    if (!result) {
+        fprintf(stderr, "Erreur : allocation mémoire échouée dans concat()\n");
+        return NULL;
+    }
+
+    result[0] = '\0'; // init string vide
+
+    va_start(args, nbArgs);
+    for (int i = 0; i < nbArgs; i++) {
+        const char *s = va_arg(args, const char *);
+        if (s != NULL) strcat(result, s);
+    }
+    va_end(args);
+
+    return result;
+}
+
+char *nodeName() { /*genere un nom unique*/
+    char buffer[32];
+    snprintf(buffer, sizeof(buffer), "node_%d", numNode++);
+    return strdup(buffer);  
+}
+
+char *nodeCreat(char *nom, char *label, int numero) { /*applique le style nécésaire au noeud*/
+    const char *shape;
+    const char *color;
+    const char *style = "solid";
+
+    switch (numero) {
+        case 1: shape = "invtrapezium"; color = "blue";  break;
+        case 2: shape = "trapezium";    color = "blue";  break;
+        case 3: shape = "diamond";      color = "black"; break;
+        case 4: shape = "box";          color = "black"; break;
+        case 5: shape = "septagon";     color = "black"; break;
+        default:shape = "ellipse";      color = "black"; break;
+    }
+
+    return concat(10,
+        nom, 
+        "[shape=", shape,
+        " label=\"", label,
+        "\" style=", style,
+        " color=", color,
+        "]\n"
+    );
+}
---
+++ utils/arbres.h
@@ -0,0 +1,9 @@
+#ifndef ARBRE_H
+#define ARBRE_H
+
+extern int numNode;
+char *concat(int nbArgs, ...);
+char *nodeName(void);
+char *nodeCreat(char *nom, char *label, int numero);
+
+#endif // ARBRE_H
