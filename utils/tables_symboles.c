d8995adc520fcae579047d6e85089352474efe4d
Bence-dp <bencediplacido16@gmail.com>
2025-04-19 12:31:25 +0200

    ajustement tables de symboles

--- utils/tables_symboles.c
+++ utils/tables_symboles.c
@@ -25,13 +25,83 @@ table_t* top_table() { /* sommet de la pile / derniere tables de symboles */
     return pile;
 }
 
-symbole_t* ajouter(table_t *table, char *nom) { /* ajouter un symbole a la table */
-    symbole_t *s = (symbole_t *)malloc(sizeof(symbole_t));
+symbole_t* ajouter(table_t *table, char *nom, int aritee, int nbDimensions, int *tailles, type_t type, int is_function) {
+    symbole_t *s = malloc(sizeof(symbole_t));
     s->nom = strdup(nom);
+    s->aritee = aritee;
+    s->nbDimensions = nbDimensions;
+    s->is_function = is_function;
+    s->type = type;
+    s->tailles = NULL;
+
+    if (nbDimensions > 0 && tailles != NULL) {
+        s->tailles = malloc(nbDimensions * sizeof(int));
+        for (int i = 0; i < nbDimensions; i++) {
+            s->tailles[i] = tailles[i];
+        }
+    }
+
     s->suivant = table->symboles;
     table->symboles = s;
     return s;
 }
+
+
+void declarer(char *nom, int aritee, int nbDimensions,int *tailles, type_t type,int is_function) {
+    if (pile == NULL) {
+        fprintf(stderr, "Erreur : pile de symboles vide !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (rechercher(pile, nom)) {
+        fprintf(stderr, "Erreur : redéclaration de la variable %s\n", nom);
+        exit(EXIT_FAILURE);
+    }
+
+    ajouter(pile, nom, aritee, nbDimensions, *tailles,type,is_function);
+}
+
+void verifier_declaration(char *nom) {
+    if (!rechercher_dans_pile(nom)) {
+        fprintf(stderr, "Erreur : %s n’a pas été déclarée !\n", nom);
+        exit(EXIT_FAILURE);
+    }
+}
+void verifier_dimensions(char *nom, int nbDemandees) {
+    symbole_t *s = rechercher_dans_pile(nom);
+    if (!s) {
+        fprintf(stderr, "Erreur : %s n’a pas été déclarée !\n", nom);
+        exit(EXIT_FAILURE);
+    }
+
+    if (s->nbDimensions != nbDemandees) {
+        fprintf(stderr, "Erreur : Mauvais nombre de dimensions pour %s. Attendu : %d, fourni : %d\n",
+                nom, s->nbDimensions, nbDemandees);
+    }
+}
+void verifier_tailles(char *nom, int nbDemandees, int *taillesDemandees) {
+    symbole_t *s = rechercher_dans_pile(nom);
+    if (!s) {
+        fprintf(stderr, "Erreur : %s n’a pas été déclarée !\n", nom);
+        exit(EXIT_FAILURE);
+    }
+
+    if (s->nbDimensions != nbDemandees) {
+        fprintf(stderr, "Erreur : Mauvais nombre de dimensions pour %s\n", nom);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < nbDemandees; i++) {
+        if (s->tailles[i] != taillesDemandees[i]) {
+            fprintf(stderr, "Erreur : Dimension %d incorrecte pour %s. Attendu : %d, fourni : %d\n",
+                    i + 1, nom, s->tailles[i], taillesDemandees[i]);
+        }
+    }
+}
+
+
+
+
 
 symbole_t* rechercher(table_t *table, char *nom) {  /*  rechercher un symbole dans la table */
     for (symbole_t *s = table->symboles; s != NULL; s = s->suivant) {
@@ -40,6 +110,14 @@ symbole_t* rechercher(table_t *table, char *nom) {  /*  rechercher un symbole da
     }
     return NULL;
 }
+symbole_t* rechercher_dans_pile(char *nom) {
+    for (table_t *t = pile; t != NULL; t = t->suivant) {
+        symbole_t *s = rechercher(t, nom);
+        if (s) return s;
+    }
+    return NULL;
+}
+
 
 void supprimer(table_t *table, char *nom) { /*  supprimer un symbole dans la table et vide la mem alloué */
     symbole_t **pp = &table->symboles;
@@ -73,3 +151,14 @@ int taille_type(type_t type) {  /* taille des differents types */
         default: return 0;
     }
 }
+
+void afficher_pile() {
+    int profondeur = 0;
+    for (table_t *t = pile; t != NULL; t = t->suivant) {
+        printf("Table %d :\n", profondeur++);
+        for (symbole_t *s = t->symboles; s != NULL; s = s->suivant) {
+            printf("\tNom: %s | Aritee: %d | Dim: %d\n", s->nom, s->aritee, s->nbDimensions);
+        }
+    }
+}
+
--- utils/tables_symboles.h
+++ utils/tables_symboles.h
@@ -3,29 +3,38 @@
 
 typedef enum { INT, STRING } type_t;
 
-typedef struct symbole { /* symbole */
+
+typedef struct symbole {
     char *nom;
-    type_t type;
-    int taille;
-    int position;
+    int aritee;
+    int nbDimensions;
+    int *tailles;
+    int is_function;
+    type_t type; // si besoin
     struct symbole *suivant;
 } symbole_t;
 
-typedef struct table { 
+typedef struct table {
     symbole_t *symboles;
     struct table *suivant;
 } table_t;
 
+
 /* Fonctions pour la gestion des tables / symboles */
-symbole_t* ajouter(table_t *table, char *nom);
+symbole_t* ajouter(table_t *table, char *nom, int aritee, int nbDimensions, int *tailles, type_t type, int is_function);
+void declarer(char *nom, int aritee, int nbDimensions,int *tailles, type_t type,int is_function);
+void verifier_declaration(char *nom);
+void verifier_dimensions(char *nom, int nbDemandees);
+void verifier_tailles(char *nom, int nbDemandees, int *taillesDemandees);
 symbole_t* rechercher(table_t *table, char *nom);
 void supprimer(table_t *table, char *nom);
 int taille_type(type_t type);
 
 /* Fonctions pour gérer la pile des tables de symboles */
+symbole_t* rechercher_dans_pile(char *nom);
 void push_table();
 void pop_table();
 table_t* top_table();
 void detruire_table(table_t *table);
-
+void afficher_pile();
 #endif
